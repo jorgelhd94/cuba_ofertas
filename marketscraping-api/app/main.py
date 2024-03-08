2fea8581c2358c622113b2601ae4218e992c3c5b
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-03-08 17:58:13 -0500

    add logging

--- marketscraping-api/app/main.py
+++ marketscraping-api/app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from app.routers.search.router import search_router
+from fastapi.logger import logger
+import logging
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+logger.handlers = gunicorn_logger.handlers
+if __name__ != "__main__":
+    logger.setLevel(gunicorn_logger.level)
+else:
+    logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
