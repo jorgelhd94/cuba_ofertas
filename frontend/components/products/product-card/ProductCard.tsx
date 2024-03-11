1f1045bf88144c165803a1bd646134fae49c504f
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-03-11 17:04:37 -0400

    add url to card

--- frontend/components/products/product-card/ProductCard.tsx
+++ frontend/components/products/product-card/ProductCard.tsx
@@ -8,21 +8,33 @@ type ProductCardProps = {
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <Card shadow="sm" isPressable onPress={() => console.log("item pressed")}>
+    <Card shadow="sm" isPressable>
       <CardBody className="overflow-visible p-0">
         <Image
           shadow="sm"
           radius="lg"
           width="100%"
-          alt={product.name}        
+          alt={product.name}
           className="w-full object-cover h-[240px]"
           src={product.image_url}
         />
       </CardBody>
       <CardFooter className="flex flex-col items-start justify-start gap-2">
-        <b className="text-start text-small text-primary-800">{product.name}</b>
-        <p className="text-start text-small font-medium">Marca: {product.manufacture.name}</p>
-        <p className="font-bold text-lg">{product.current_price} {product.currency}</p>
+        <a href={product.product_url} target="_blank" rel="noopener noreferrer">
+          <b className="text-start text-small text-primary-800">
+            {product.name}
+          </b>
+        </a>
+
+        <a href={product.manufacture.url} target="_blank" rel="noopener noreferrer">
+          <p className="text-start text-small font-medium">
+            Marca: {product.manufacture.name}
+          </p>
+        </a>
+
+        <p className="font-bold text-lg">
+          {product.current_price} {product.currency}
+        </p>
       </CardFooter>
     </Card>
   );
