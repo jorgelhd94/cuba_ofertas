43f6e1995eb49a93e07f7c5a3fdb523ec21b745d
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-03-12 11:03:32 -0400

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from app.routers.search.router import search_router
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.logger import logger
 import logging
 
@@ -10,7 +11,21 @@ if __name__ != "__main__":
 else:
     logger.setLevel(logging.DEBUG)
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:80", # Dirección de tu aplicación Next.js en desarrollo
+    "http://localhost:80", # Dirección de tu aplicación Next.js en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(search_router, prefix="/api/v1")
 
