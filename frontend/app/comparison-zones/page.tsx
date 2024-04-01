3279f7f7069150cbff4614f2e54d05fceb4f3fdd
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-04-01 19:54:43 -0400

    fix page

--- frontend/app/comparison-zones/page.tsx
+++ frontend/app/comparison-zones/page.tsx
@@ -12,27 +12,14 @@ export default async function ComparisonZonesPage() {
   return (
     <div className="min-h-max lg:min-h-screen bg-gradient-to-tr from-white to-slate-200 dark:bg-gradient-to-b dark:from-slate-800 via-transparent dark:to-black">
       <div className="py-24 lg:py-12 container mx-auto max-w-screen-xl">
-        {comparisonZones.length ? (
-          <div className="w-full md:pt-8 space-y-8">
-            <h1 className="text-3xl w-full text-center">
-              Zonas de comparación
-            </h1>
-            <div className="gap-4 flex flex-col sm:flex-row items-center justify-evenly flex-wrap">
-              {comparisonZones.map((zone) => {
-                return (
-                  <ComparisonZoneCard key={zone.id} comparisonZone={zone} />
-                );
-              })}
-            </div>
+        <div className="w-full md:pt-8 space-y-8">
+          <h1 className="text-3xl w-full text-center">Zonas de comparación</h1>
+          <div className="gap-4 flex flex-col sm:flex-row items-center justify-evenly flex-wrap">
+            {comparisonZones.map((zone) => {
+              return <ComparisonZoneCard key={zone.id} comparisonZone={zone} />;
+            })}
           </div>
-        ) : (
-          <div className="mt-12">
-            <EmptyMsg
-              title="Opps!!"
-              message="No se han creado Zonas de comparación"
-            />
-          </div>
-        )}
+        </div>
       </div>
     </div>
   );
