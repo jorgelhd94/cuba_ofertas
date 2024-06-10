85c3208cd0015863d316beb7d6da3b207c843d7e
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-06-10 18:43:32 -0400

    update crontab

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,5 +144,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRONJOBS = [
-    ('42 22 * * *', 'apps.search.tasks.update_database_sm23'),
+    ('46 22 * * *', 'apps.search.tasks.update_database_sm23'),
 ]
