b9952b216cad2d5b0c3663a8835a371853e84057
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-06-10 18:38:45 -0400

    add cron

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crontab',
     'corsheaders',
     'rest_framework',
     'apps.search',
@@ -141,3 +142,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRONJOBS = [
+    ('42 22 * * *', 'apps.search.tasks.update_database_sm23'),
+]
