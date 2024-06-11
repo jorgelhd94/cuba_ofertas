e89abb093ddbcc8a9f738be794a415eec0c79f57
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-06-11 16:49:15 -0400

    test authentication

--- backend/apps/search/urls.py
+++ backend/apps/search/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('search/', views.SearchView.as_view(), name='search'),
     path('update-database/', views.UpdateDatabaseView.as_view(), name='update-database'),
+    path('test-auth/', views.TestAuthView.as_view(), name='test-auth'),
 ]
