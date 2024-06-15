8a66c7be873efc59961b0c611fd5844660306ae2
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-06-14 20:49:15 -0400

    fix frontend

--- frontend/app/api/comparison-zones/route.ts
+++ frontend/app/api/comparison-zones/route.ts
@@ -1,16 +1,17 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
-  const res = await fetch(
-    process.env.NEXT_PUBLIC_API_URL! + "api/v1/comparison_zones/",
-    { cache: "no-store" }
-  );
+  // const res = await fetch(
+  //   process.env.NEXT_PUBLIC_API_URL! + "api/v1/comparison_zones/",
+  //   { cache: "no-store" }
+  // );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch data");
+  // }
 
-  const data = await res.json();
+  // const data = await res.json();
+  const data = {};
 
   revalidatePath("/comparison-zones");
 
--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -1,11 +1,14 @@
 import { SearchSection } from "@/components/search/SearchSection/SearchSection";
+import { Suspense } from "react";
 
 export default async function SearchPage() {
   return (
     <div className=" container mx-auto max-w-screen-xl">
       <div className="w-full md:pt-8 space-y-8">
         <h1 className="text-3xl w-full text-center">Búsqueda general</h1>
-        <SearchSection />
+        <Suspense>
+          <SearchSection />
+        </Suspense>
       </div>
     </div>
   );
