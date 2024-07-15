ef64d23cae560d9fc1002fd7d5b760a43310eed6
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-07-15 16:37:04 -0400

    add test functions

--- backend/apps/integrations/models.py
+++ backend/apps/integrations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TKC_Credentials(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tkc_credentials')
+
+    def __str__(self):
+        return self.username
---
+++ backend/apps/integrations/tkc/tkc_integration.py
@@ -0,0 +1,10 @@
+login_url = "https://www.almendarestravel.com/products/backend/login"
+
+test_credentials = {
+    "username": "victor.leon",
+    "password": "victor*2023"
+}
+
+
+def test_auth():
+    pass
