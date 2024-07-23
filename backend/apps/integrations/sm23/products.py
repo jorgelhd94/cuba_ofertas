13990651626976ac38fe9b3a76bce89030b17f5b
Raidel <rraidel85@gmail.com>
2024-07-23 16:46:59 -0400

    fetch and create providers from sm23

--- backend/apps/integrations/sm23/products.py
+++ backend/apps/integrations/sm23/products.py
@@ -1,9 +1,6 @@
 import requests
 
-from apps.product.models import Manufacture
-
-def create_manufacture(manufacture):
-    pass
+from apps.product.models import Manufacture, Provider
 
 def add_if_no_exists(item, item_list):
     new_list = item_list
@@ -18,6 +15,11 @@ def update_products(headers, shop, proxy=None):
     
     print("Starting to fetch products from Supermarket 23...")
     try:
+        manufactures = []
+        providers = []
+        categories = []
+        products_id = []  
+          
         first_response = requests.get(url, headers=headers)
         
         if first_response.status_code == 200:
@@ -27,8 +29,6 @@ def update_products(headers, shop, proxy=None):
                 "language":"SPA",
                 "limit": total
             }
-
-            manufactures = []
             
             response = requests.get(url, headers=headers, params=params)
             
@@ -37,20 +37,43 @@ def update_products(headers, shop, proxy=None):
                 products = products_data.get("products")
                 
                 for product in products:
-                    manufacture_name = product["Brand"]
-                    manufactures, manufacture_added = add_if_no_exists(manufacture_name, manufactures)
-                    if manufacture_added:
-                        manufacture_url = f"https://www.supermarket23.com/es/productos/bp?q={manufacture_name}"
-                        new_manufacture = {
-                                "name": manufacture_name,
-                                "url": manufacture_url,
-                                "shop": shop
-                            }
-                        Manufacture.objects.update_or_create(
-                            name=manufacture_name,
-                            shop=shop,
-                            defaults=new_manufacture
-                        )
+                    # Manufactures
+                    # manufacture_name = product.get("Brand")
+                    # if manufacture_name:
+                    #     manufactures, manufacture_added = add_if_no_exists(manufacture_name, manufactures)
+                    #     if manufacture_added:
+                    #         manufacture_url = f"https://www.supermarket23.com/es/productos/bp?q={manufacture_name}"
+                    #         new_manufacture = {
+                    #                 "name": manufacture_name,
+                    #                 "url": manufacture_url,
+                    #                 "shop": shop
+                    #             }
+                    #         Manufacture.objects.update_or_create(
+                    #             name=manufacture_name,
+                    #             shop=shop,
+                    #             defaults=new_manufacture
+                    #         )
+                    # Providers
+                    provider_name = product.get("ProviderName")
+                    if provider_name:
+                        provider_id = product.get("ProviderId")
+                        providers, provider_added = add_if_no_exists(provider_name, providers)
+                        if provider_added:
+                            provider_url = f"https://www.supermarket23.com/es/productos/proveedor?q={provider_name}"
+                            new_provider = {
+                                    "name": provider_name,
+                                    "url": provider_url,
+                                    "provider_id": provider_id,
+                                    "shop": shop
+                                }
+                            Provider.objects.update_or_create(
+                                name=provider_name,
+                                shop=shop,
+                                defaults=new_provider
+                            )
+                    # Categories
+                    category_id = product.get("CategoryId")
+                    category_name = product.get("SpaCategoryName")    
                 print(len(products))    
                 print("------Products sm23 process completed successfully------")
             else:
