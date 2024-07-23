792ec6e31b60f3f36a3ead7f61a400e6ca8f0d35
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2024-07-23 14:42:42 -0400

    fix logo

--- frontend/components/shared/logo.tsx
+++ frontend/components/shared/logo.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="flex items-center gap-2 select-none">
-      {/* <Image src="/logo.svg" alt="Logo" width={32} height={32} /> */}
+      <Image src="/logo.svg" alt="Logo" width={32} height={32} />
       <h2 className="font-bold text-xl flex flex-col gap-0 leading-none m-0 p-0">
-        {/* <span className="text-sm leading-none text-primary-800">Cuba</span> */}
-        SPY-Market
+        <span className="text-sm leading-none text-primary-800">Cuba</span>
+        Ofertas
       </h2>
     </div>
   );
