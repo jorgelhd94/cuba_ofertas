7f52463ec23df425dfa4fbb77d716bbf4645fdb3
Raidel <rraidel85@gmail.com>
2024-07-23 14:23:28 -0400

    bring and create manufactures from sm23 api

--- backend/apps/integrations/sm23/products.py
+++ backend/apps/integrations/sm23/products.py
@@ -1,29 +1,60 @@
 import requests
 
-def proccess_products():
+from apps.product.models import Manufacture
+
+def create_manufacture(manufacture):
     pass
+
+def add_if_no_exists(item, item_list):
+    new_list = item_list
+    added = False
+    if item not in item_list:
+        new_list.append(item)
+        added = True
+    return new_list, added
 
 def update_products(headers, shop, proxy=None):
     url = "https://apitreewsearchengine.treew.com/products"
     
-    params = {
-        "language":"SPA",
-    }
-
-    
     print("Starting to fetch products from Supermarket 23...")
     try:
-        response = requests.get(url, headers=headers)
+        first_response = requests.get(url, headers=headers)
         
-        if response.status_code == 200:
-            products_data = response.json()
-            total = products_data.get("total")
-            products = products_data.get("products")
+        if first_response.status_code == 200:
+            total = first_response.json().get("total")
             
-            return products
-            print("------Products sm23 process completed successfully------")
-        else:
-            print(f"Failed to fetch providers. Status code: {response.status_code}")
+            params = {
+                "language":"SPA",
+                "limit": total
+            }
+
+            manufactures = []
+            
+            response = requests.get(url, headers=headers, params=params)
+            
+            if response.status_code == 200:
+                products_data = response.json()
+                products = products_data.get("products")
+                
+                for product in products:
+                    manufacture_name = product["Brand"]
+                    manufactures, manufacture_added = add_if_no_exists(manufacture_name, manufactures)
+                    if manufacture_added:
+                        manufacture_url = f"https://www.supermarket23.com/es/productos/bp?q={manufacture_name}"
+                        new_manufacture = {
+                                "name": manufacture_name,
+                                "url": manufacture_url,
+                                "shop": shop
+                            }
+                        Manufacture.objects.update_or_create(
+                            name=manufacture_name,
+                            shop=shop,
+                            defaults=new_manufacture
+                        )
+                print(len(products))    
+                print("------Products sm23 process completed successfully------")
+            else:
+                print(f"Failed to fetch providers. Status code: {response.status_code}")
     except Exception as e:
         print(f"An error occurred: {e}")
     
